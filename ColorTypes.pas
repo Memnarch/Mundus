8c2a4e20483af063eb0d0b544de92e069687387c
Memnarch <Sebal007@freenet.de>
2013-08-10 15:58:56 +0200

    Added RGB32 function to fill record

--- ColorTypes.pas
+++ ColorTypes.pas
@@ -9,6 +9,16 @@ type
   TRGB32Array = packed array[0..MaxInt div SizeOf(TRGB32)-1] of TRGB32;
   PRGB32Array = ^TRGB32Array;
 
+  function RGB32(R, G, B, A: Byte): TRGB32;
+
 implementation
+
+function RGB32(R, G, B, A: Byte): TRGB32;
+begin
+  Result.R := R;
+  Result.G := G;
+  Result.B := B;
+  Result.A := A;
+end;
 
 end.
