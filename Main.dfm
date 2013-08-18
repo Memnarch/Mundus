ec03ea7f6276bb11d319efbb1a6168791d68ec03
Memnarch <Sebal007@freenet.de>
2013-08-18 18:49:13 +0200

    Screen is now almost freely resizeable(RenderTargetSize is always set to something you can divide by the QuadSize, default 8)

--- Main.dfm
+++ Main.dfm
@@ -4,37 +4,21 @@ object Form1: TForm1
   Caption = 'SoftwareRenderer'
   ClientHeight = 542
   ClientWidth = 512
-  Color = clBtnFace
+  Color = clNone
+  DoubleBuffered = True
   Font.Charset = DEFAULT_CHARSET
   Font.Color = clWindowText
   Font.Height = -11
   Font.Name = 'Tahoma'
   Font.Style = []
+  FormStyle = fsStayOnTop
   OldCreateOrder = False
   OnClose = FormClose
   OnCreate = FormCreate
   OnKeyDown = FormKeyDown
+  OnResize = FormResize
   PixelsPerInch = 96
   TextHeight = 13
-  object RenderInfo: TLabel
-    Left = 8
-    Top = 512
-    Width = 174
-    Height = 19
-    Caption = 'Some Render Infos Here'
-    Font.Charset = DEFAULT_CHARSET
-    Font.Color = clWindowText
-    Font.Height = -16
-    Font.Name = 'Tahoma'
-    Font.Style = []
-    ParentFont = False
-  end
-  object GameScreen: TPaintBox
-    Left = 0
-    Top = 0
-    Width = 512
-    Height = 512
-  end
   object GameTimer: TTimer
     Enabled = False
     Interval = 1
--- Main.pas
+++ Main.pas
@@ -8,13 +8,12 @@ uses
 
 type
   TForm1 = class(TForm)
-    RenderInfo: TLabel;
     GameTimer: TTimer;
-    GameScreen: TPaintBox;
     procedure FormCreate(Sender: TObject);
     procedure GameTimerTimer(Sender: TObject);
     procedure FormClose(Sender: TObject; var Action: TCloseAction);
     procedure FormKeyDown(Sender: TObject; var Key: Word; Shift: TShiftState);
+    procedure FormResize(Sender: TObject);
   private
     { Private-Deklarationen }
     FSoftwareRenderer: TSoftwareRenderer;
@@ -22,8 +21,10 @@ type
     FWatch: TStopWatch;
     FMinFPS, FMaxFPS: Integer;
     FCube: TCube;
+    procedure HandleAfterFrame(ACanvas: TCanvas);
   public
     { Public-Deklarationen }
+    procedure SetResolution(AWidth, AHeight: Integer);
   end;
 
 var
@@ -47,11 +48,9 @@ begin
   FCube.Position := Vector(0, 0, 132);
   FSoftwareRenderer := TSoftwareRenderer.Create();
   FSoftwareRenderer.MeshList.Add(FCube);
-  FSoftwareRenderer.SetResolution(1280, 720);
-  GameScreen.Width := 1280;
-  GameScreen.Height := 720;
-  ClientWidth := 1280;
-  ClientHeight := 800;
+
+  FSoftwareRenderer.OnAfterFrame := HandleAfterFrame;
+  SetResolution(1280, 720);
 //  LCube := TCube.Create();
 //  LCube.Position := Vector(-20, 0, LCube.Position.Z);
 //  FSoftwareRenderer.MeshList.Add(LCube);
@@ -81,19 +80,58 @@ begin
   begin
     FCube.Position := Vector(FCube.Position.X, FCube.Position.Y, FCube.Position.Z - 3);
   end;
+  if (Key = VK_ESCAPE) and (WindowState = wsMaximized) then
+  begin
+    WindowState := wsNormal;
+  end;
+end;
+
+procedure TForm1.FormResize(Sender: TObject);
+begin
+  if WindowState = wsMaximized then
+  begin
+    BorderStyle := bsNone;
+    ClientWidth := Screen.Width;
+    ClientHeight := Screen.Height;
+    Left := 0;
+    Top := 0;
+  end
+  else
+  begin
+    BorderStyle := bsSizeable;
+  end;
+  SetResolution(ClientWidth, ClientHeight);
 end;
 
 procedure TForm1.GameTimerTimer(Sender: TObject);
+begin
+  FSoftwareRenderer.RenderFrame(Canvas);
+end;
+
+procedure TForm1.HandleAfterFrame(ACanvas: TCanvas);
 var
   LFPS: Integer;
 begin
-  FSoftwareRenderer.RenderFrame(GameScreen.Canvas);
+  ACanvas.Brush.Color := clWhite;
   LFPS := FSoftwareRenderer.GetCurrentFPS;
-  FMaxFPS := Max(FMaxFPS, LFPS);
-  FMinFPS := Min(FMinFPS, LFPS);
-  RenderInfo.Caption := 'FPS: ' + IntToStr(LFPS) + ' Min-Fps: ' +
-      IntToStr(FMinFPS) + ' Max-Fps: ' + IntToStr(FMaxFPS) + ' PolyCount: ' +
-      IntToStr(FSoftwareRenderer.GetCurrentPolyCount);
+  if LFPS > 0 then
+  begin
+    FMaxFPS := Max(FMaxFPS, LFPS);
+    FMinFPS := Min(FMinFPS, LFPS);
+    ACanvas.TextOut(10, 10, IntToStr(FSoftwareRenderer.ResolutionX) + 'x' + IntToStr(FSoftwareRenderer.ResolutionY));
+    ACanvas.TextOut(10, 30, 'FPS: ' + IntToStr(LFPS) + ' Min-Fps: ' +
+        IntToStr(FMinFPS) + ' Max-Fps: ' + IntToStr(FMaxFPS) + ' PolyCount: ' +
+        IntToStr(FSoftwareRenderer.GetCurrentPolyCount));
+  end;
+end;
+
+procedure TForm1.SetResolution(AWidth, AHeight: Integer);
+begin
+  FSoftwareRenderer.SetResolution(AWidth div FSoftwareRenderer.QuadSize * FSoftwareRenderer.QuadSize,
+    AHeight div FSoftwareRenderer.QuadSize * FSoftwareRenderer.QuadSize);
+  ClientWidth := AWidth;
+  ClientHeight := AHeight;
+  Caption := 'SoftwareRenderer ' + IntToSTr(AWidth) + 'x' + IntToStr(AHeight);
 end;
 
 end.
